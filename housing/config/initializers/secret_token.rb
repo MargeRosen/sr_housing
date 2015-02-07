fb4d7f11419c44ff4e562162718fea7e1802b5bc
Marge Rosen <marge@masterclassu.com>
2015-02-06 21:47:57 -0800

    First commit

---
+++ housing/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Housing::Application.config.secret_key_base = 'c84858a3a6f4f510e0b0bc97337cc7fc4cf024709ccc82de9a1578987ac8c314670ab93a49c100386d3315f29e3b23354e37456904b56eb2ddced2ff620a6d32'
---
+++ housing/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Housing::Application.config.session_store :cookie_store, key: '_housing_session'
